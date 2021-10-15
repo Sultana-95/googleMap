3698f704a1de1b7b1bc0fefdaddaca67164def11
Sultana-95 <alburtmani59@gmail.com>
2021-10-14 23:51:18 -1200

    map

---
+++ resources/views/map.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+    <title>Laravel Google Maps Example</title>
+    </head>
+<body>
+    <div style="width: 100%; height: 500px;">
+        {!! Mapper::render() !!}
+    </div>
+</body>
+</html>
